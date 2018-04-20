4c1d214455d2d95670ed5013b637b42eb73de271
Segun Olumide <victorwonex@gmamail.com>
2018-04-20 19:20:48 +0100

    Count down is complete, but when changing the alarm time the timer goes faster

--- js/timerr.js
+++ js/timerr.js
@@ -1,88 +1,76 @@
 var app = new Vue({
     el: "#app-timer",
     data: {
-        fullDate: "",
         alarmTime: "",
         alarmTimeHour: "",
         alarmTimeMinute: "",
         alarmTimeSeconds: "",
-        timeDiff: null,
-        times: [],
-        mymonths: [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-            "August",
-            "September",
-            "October",
-            "November",
-            "December"
-        ],
-        timeinterval: undefined,
+        timeDiff: "00:00:00",
+        interval: 1000,
+        countDown: undefined,
     },
     methods: {
-        update() {
-            // this.setAlarm();
-            // this.timeinterval = clearInterval();
-            clearTimeout(this.setAlarm);
 
-            // this.timeinterval = setInterval(this.setAlarm, 1000);
-            // setInterval(function() {
-            //     duration = moment.duration(duration - interval, "milliseconds");
-            //     $(".countdown").text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds());
-            // }, interval);
+        resetInterval() {
+
+            clearTimeout(this.countDown);
         },
 
         setAlarm() {
 
-            // this.update();
             this.timeDiff = null;
 
+            // this shows the hours, minutes and seconds inputs
             this.alarmTime = this.alarmTimeHour + ":" + this.alarmTimeMinute + ":" + this.alarmTimeSeconds;
-            // var dateString = "Tue May 16 2017 15:34:23 GMT+0100 (FLE Daylight Time)";
-            var dateString = (moment().format("ddd") + " " + moment().format("MMMM") + " " + moment().format("D") + " " + moment().format("YYYY") + " 15:34:23 GMT+0100 (WAT)").toString();
-            var m = moment(dateString, "ddd MMMM D YYYY HH:mm:ss ZZ");
-            // Use moment(Date) if your input is a JS Date
-            //var m = moment(date);
-            // m.set({ h: 11, m: 11 });
-            m.set({
+
+
+            // Getting the Current Date
+            var currentDate = moment().format("ddd") + " " + moment().format("MMM") + " " + moment().format("D") + " " + moment().format("YYYY") + " " + moment().format("HH:mm:ss") + " GMT+0100 (WAT)";
+
+            // Setting Date format
+            var setDate = moment(currentDate, "ddd MMMM D YYYY HH:mm:ss ZZ");
+
+            // setting my Hour, Minutes and seconds binding it with my Input
+            setDate.set({
                 h: this.alarmTimeHour,
                 m: this.alarmTimeMinute,
                 s: this.alarmTimeSeconds
             });
-            console.log(m.format('x'));
-            console.log(m.toDate().toString());
-            console.log(dateString);
 
+            // comparing current date with my new date
+            console.log(setDate.toDate().toString());
+            console.log(currentDate);
 
-            // Work on This
-            var eventTime = m.format("x"); // Timestamp - Sun, 21 Apr 2013 13:00:00 GMT
-            var currentTime = moment().format('x'); // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
-            // var currentTime = 1366547400; // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
+            // * Getting the time Difference
+            var eventTime = setDate.format('X'); // Timestamp - Sun, 21 Apr 2013 13:00:00 GMT
+            var currentTime = moment().format('X'); // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
             var diffTime = eventTime - currentTime;
-            var duration = moment.duration(diffTime * 1000, "milliseconds");
+            duration = moment.duration(diffTime * 1000, "milliseconds");
             var interval = 1000;
 
-            // console.log(currentTime);
-            // setInterval(function() {
-            //     duration = moment.duration(duration - interval, "milliseconds");
-            //     $(".countdown").text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds());
-            // }, interval);
-            // console.log(duration)
-
-        },
-        counting() {
-            duration = moment.duration(duration - interval, "milliseconds");
-            $(".countdown").text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds());
+            // Counting the time down 
+            this.countDown = function() {
+                duration = moment.duration(duration - interval, "milliseconds");
+                this.timeDiff = $(".countdown").text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds());
+                // this.timeDiff = duration.hours() + ":" + duration.minutes() + ":" + duration.seconds();
+            }
+            this.resetInterval();
+            setInterval(this.countDown, this.interval);
         }
-    },
-    // created: function() {
-    // }
+    }
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 /***
  * enter time in input 
@@ -94,7 +82,20 @@ var app = new Vue({
  * 
  * 
  * 
- * 
+ * // Work on This
+ var eventTime = m.format("x"); // Timestamp - Sun, 21 Apr 2013 13:00:00 GMT
+ var currentTime = moment().format('x'); // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
+ // var currentTime = 1366547400; // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
+ var diffTime = eventTime - currentTime;
+ duration = moment.duration(diffTime * 1000, "milliseconds");
+ var interval = 1000;
+
+ // console.log(currentTime);
+ // setInterval(function() {
+ //     duration = moment.duration(duration - interval, "milliseconds");
+ //     $(".countdown").text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds());
+ // }, interval);
+ // console.log(duration)
  * 
  * 
  */
