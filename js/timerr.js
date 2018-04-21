198426d5c4f15fe571cac840a19dd47350f59672
Segun Olumide <victorwonex@gmamail.com>
2018-04-21 21:41:55 +0100

    Updating Hours Of the Day Greeting

--- js/timerr.js
+++ js/timerr.js
@@ -1,31 +1,29 @@
 var app = new Vue({
     el: "#app-timer",
     data: {
+        hourformat: "",
+        clock: "",
+        clockAnimate: "",
+        TodaysDate: "",
         alarmTime: "",
         alarmTimeHour: "",
         alarmTimeMinute: "",
         alarmTimeSeconds: "",
         timeDiff: "00:00:00",
         interval: 1000,
+        saveInterval: undefined,
+        timeInterval: undefined,
         countDown: undefined,
     },
     methods: {
-
-        resetInterval() {
-
-            clearTimeout(this.countDown);
-        },
-
         setAlarm() {
-
             this.timeDiff = null;
 
             // this shows the hours, minutes and seconds inputs
             this.alarmTime = this.alarmTimeHour + ":" + this.alarmTimeMinute + ":" + this.alarmTimeSeconds;
 
-
             // Getting the Current Date
-            var currentDate = moment().format("ddd") + " " + moment().format("MMM") + " " + moment().format("D") + " " + moment().format("YYYY") + " " + moment().format("HH:mm:ss") + " GMT+0100 (WAT)";
+            var currentDate = moment().format("ddd MMMM D YYYY HH:mm:ss ZZ");
 
             // Setting Date format
             var setDate = moment(currentDate, "ddd MMMM D YYYY HH:mm:ss ZZ");
@@ -38,25 +36,65 @@ var app = new Vue({
             });
 
             // comparing current date with my new date
-            console.log(setDate.toDate().toString());
-            console.log(currentDate);
+            // console.log(setDate.toDate().toString());
+            // console.log(currentDate);
 
             // * Getting the time Difference
-            var eventTime = setDate.format('X'); // Timestamp - Sun, 21 Apr 2013 13:00:00 GMT
-            var currentTime = moment().format('X'); // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
+            var eventTime = setDate.format("X"); // Timestamp - Sun, 21 Apr 2013 13:00:00 GMT
+            var currentTime = moment().format("X"); // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
             var diffTime = eventTime - currentTime;
             duration = moment.duration(diffTime * 1000, "milliseconds");
             var interval = 1000;
 
-            // Counting the time down 
+            // Counting the time down
             this.countDown = function() {
                 duration = moment.duration(duration - interval, "milliseconds");
-                this.timeDiff = $(".countdown").text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds());
-                // this.timeDiff = duration.hours() + ":" + duration.minutes() + ":" + duration.seconds();
-            }
+
+                if (duration.seconds() < 0) {
+                    this.timeDiff = $(".countdown").text("Time is Up !!");
+                    clearInterval(this.saveInterval);
+                    // When Countdown alarm Time is Up
+                } else if (duration.hours() || duration.minutes() || duration.seconds() > 0) {
+                    this.timeDiff = $(".countdown").text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds());
+                }
+            };
+
             this.resetInterval();
-            setInterval(this.countDown, this.interval);
+        },
+        resetInterval() {
+
+            //* Update Alarm
+            clearInterval(this.saveInterval);
+            this.saveInterval = setInterval(this.countDown, this.interval);
+        },
+        resetAlarm() {
+            //* Reseting Alarm
+            clearInterval(this.saveInterval);
+            this.timeDiff = $(".countdown").text("00:00:00");
+        },
+        getDate() {
+            // Getting the Calendar and updated Clock time
+            var calender = moment().format('dddd MMMM D YYYY');
+            var clock = moment().format('hh:mm A');
+            this.TodaysDate = calender;
+            this.clock = clock;
+
+            // Comparing Time of Day
+            var hours = moment().format('HH')
+            if (hours >= 12) {
+                if (hours >= 16) {
+                    this.hourformat = "Evening";
+                } else {
+                    this.hourformat = "Afternoon";
+                }
+            } else {
+                this.hourformat = "Morning";
+            }
         }
+
+    },
+    created() {
+        this.timeInterval = setInterval(this.getDate, 100);
     }
 });
 
@@ -82,20 +120,7 @@ var app = new Vue({
  * 
  * 
  * 
- * // Work on This
- var eventTime = m.format("x"); // Timestamp - Sun, 21 Apr 2013 13:00:00 GMT
- var currentTime = moment().format('x'); // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
- // var currentTime = 1366547400; // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
- var diffTime = eventTime - currentTime;
- duration = moment.duration(diffTime * 1000, "milliseconds");
- var interval = 1000;
-
- // console.log(currentTime);
- // setInterval(function() {
- //     duration = moment.duration(duration - interval, "milliseconds");
- //     $(".countdown").text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds());
- // }, interval);
- // console.log(duration)
+ * 
  * 
  * 
  */
